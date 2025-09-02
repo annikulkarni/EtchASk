833abfabadef6f3521d445a5633e2bb5b09c543f
aanilku2 <aanilku2@visteon.com>
2025-09-02 16:50:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+var submit = document.querySelector(".submit").addEventListener("click", (e) => {
+    var inputVal = document.querySelector("#input");
+    console.log(inputVal.value);
+    const container = document.querySelector(".sketch");
+    const row = document.createElement("div");
+    row.classList.add("row");
+    var sketchArray = [];
+    for (let i = 0; i < inputVal.value; i++)
+    {
+        console.log(i);
+        const mainPiece = document.createElement("div");
+        const piece = document.createElement("div");
+        piece.classList.add("piece");
+        mainPiece.classList.add("mainPiece");
+        if (inputVal.value <= 10)
+        {
+            piece.classList.add("gridElementBig");
+        }
+        if (inputVal.value >10&&inputVal.value<=20)
+        {
+            piece.classList.add("gridElementSmall");
+        }
+        if (inputVal.value >20&&inputVal.value<=35)
+        {
+            piece.classList.add("gridElementSmaller");
+        }
+        if(inputVal.value>35)
+        {
+            piece.classList.add("gridElementSmallest");
+        }
+        mainPiece.appendChild(piece);
+        row.appendChild(mainPiece);
+    }
+    
+    for (let i = 0; i < inputVal.value; i++)
+    {
+        const newRow = row.cloneNode(true);
+        container.appendChild(newRow);
+        sketchArray[i] = newRow;
+    }
+  
+    for (let i = 0; i < inputVal.value; i++)
+    {
+        var nodes = sketchArray[i].childNodes;
+        for (let j = 0; j < inputVal.value; j++)
+        {
+            nodes[j].childNodes[0].style.backgroundColor = "black";
+            nodes[j].childNodes[0].style.opacity = 0;
+        }
+    }
+
+    container.addEventListener("mouseover", (e) => {
+        if (e.target.classList.contains('piece')) {
+            const cS = window.getComputedStyle(e.target);
+            const bC = cS.getPropertyValue('background-color');
+            const oP = cS.getPropertyValue('opacity');
+            e.target.style.opacity =  parseFloat(oP) + 0.1;
+        }
+    })
+        
+});
